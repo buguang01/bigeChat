4c822853f337cff319133dee44d52fe3b99535fc
buguang01 <273033431@qq.com>
2020-04-15 12:06:18 +0800

    up member

---
+++ usermodels/Member.go
@@ -0,0 +1,30 @@
+package usermodels
+
+/*
+这是一个例子；
+*/
+import (
+	"time"
+
+	"github.com/buguang01/bige/event"
+)
+
+type MemberMD struct {
+	MemberID   int       //
+	UserName   string    //
+	CreateTime time.Time //
+	BanTime    time.Time //
+	HashKey    string    //
+	ServerID   int       //
+	ChanID     string    //渠道ID
+	OpenID     string    //用户唯一标识
+}
+
+func (this *MemberMD) ToJson() event.JsonMap {
+	js := make(event.JsonMap)
+	js["Name"] = this.UserName
+	js["SID"] = this.ServerID
+	js["MID"] = this.MemberID
+
+	return js
+}
---
+++ usermodels/User.go
@@ -0,0 +1,12 @@
+package usermodels
+
+type UserModel struct {
+	UserInfo string          //用户信息
+	ChatLi   map[string]bool //监听的频道
+}
+
+func NewUserModel() *UserModel {
+	md := new(UserModel)
+	md.ChatLi = make(map[string]bool)
+	return md
+}
